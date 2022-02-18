1e43904281ffa9f4ab6719c46a56128523a01f2f
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-02-18 19:43:35 -0300

    refactor(cart-item): refactors the creation of elements

--- src/js/components/cart-item.js
+++ src/js/components/cart-item.js
@@ -1,4 +1,4 @@
-import { Element, Text, getCurrencyFormatted } from "../utils/index.js"
+import { Element, getCurrencyFormatted } from "../utils/index.js"
 
 const modifiers = {
   'Mega-Sena': 'cart__item-game-name--green',
@@ -10,34 +10,25 @@ function createCartItem (item, handleDeleteBetOnCart) {
   const $li = Element('li')
   $li.classList.add('cart__item')
   const $button = Element('button')
-  $button.classList.add('cart__item-action-delete')
-  const $iconDelete = Element('i')
-  $iconDelete.classList.add('cart__item-action-delete-icon')
-  $button.appendChild($iconDelete)
-  const $gameInfoContainer = Element('div')
-  $gameInfoContainer.classList.add('cart__item-game-info', modifiers[item.type])
-  const $pElNumbers = Element('p')
-  $pElNumbers.classList.add('cart__item-game-numbers')
-  const textNodeNumbers = Text(item.numbers?.join(', ') ?? '-')
-  $pElNumbers.appendChild(textNodeNumbers)
-  const $pElTypeNameAndPrice = Element('p')
-  const $typeName = Element('span')
-  $typeName.classList.add('cart__item-game-name', modifiers[item.type])
-  const textNodeTypeName = Text(item.type+' ')
-  $typeName.appendChild(textNodeTypeName)
-  $pElTypeNameAndPrice.appendChild($typeName)
-  const $price = Element('span')
-  $price.classList.add('cart__item-game-value')
-  const textNodePrice = Text(getCurrencyFormatted(item.price))
-  $price.appendChild(textNodePrice)
-  $pElTypeNameAndPrice.appendChild($price)
-
-  $gameInfoContainer.appendChild($pElNumbers)
-  $gameInfoContainer.appendChild($pElTypeNameAndPrice)
   $li.appendChild($button)
-  $li.appendChild($gameInfoContainer)
-
+  $button.classList.add('cart__item-action-delete')
   $button.addEventListener('click', handleDeleteBetOnCart(item.id))
+  $button.insertAdjacentHTML('beforeend', `
+      <i class="cart__item-action-delete-icon"></i>
+  `)
+  $li.insertAdjacentHTML('beforeend', `
+    <div class="cart__item-game-info ${modifiers[item.type]}">
+      <p class="cart__item-game-numbers">
+        ${item.numbers.join(', ')}
+      </p>
+      <p>
+        <span class="cart__item-game-name ${modifiers[item.type]}">
+          ${item.type+' '}
+        </span>
+        <span class="cart__item-game-value">${getCurrencyFormatted(item.price)}</span>
+      </p>
+    </div>
+  `);
 
   return $li
 }
